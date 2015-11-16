7a79836ba51e9d039160f34e3ce174cbb0cf0370
Joshua Boyd <joishi@gmail.com>
2015-11-15 17:56:43 -0800

    Refactoring a bit..  Defining a basic db in the Base

--- Spikes/SQLAlchemy/sql_alchemy/orm_base.py
+++ Spikes/SQLAlchemy/sql_alchemy/orm_base.py
@@ -8,11 +8,11 @@ class CarMake(Base):
     __tablename__ = "car_make"
 
     car_make_id = Column(BigInteger, primary_key=True)
-    car_make_name = Column(String(60), nullable=False)
+    make_name = Column(String(60), nullable=False)
 
     def __repr__(self):
         return "<CarMake(%r, %r)>" % (
-            self.car_make_id, self.car_make_name
+            self.car_make_id, self.make_name
         )
 
 class CarModel(Base):
@@ -20,4 +20,34 @@ class CarModel(Base):
 
     car_model_id = Column(BigInteger, primary_key=True)
     car_make_id = Column(BigInteger, ForeignKey(CarMake.car_make_id), nullable=False)
-    car_model_name = Column(String(60), nullable=False)
+    model_name = Column(String(60), nullable=False)
+
+    def __repr__(self):
+        return "<CarModel(%r, %r, %r)>" % (
+            self.car_model_id, self.car_make_id, self.model_name
+        )
+
+class CarOwner(Base):
+    __tablename__ = "car_owner"
+
+    car_owner_id = Column(BigInteger, primary_key=True)
+    first_name = Column(String(60), nullable=False)
+    last_name = Column(String(60), nullable=False)
+
+    def __repr__(self):
+        return "<CarOwner(%r, %r, %r)>" % (
+            self.car_owner_id, self.first_name, self.last_name
+        )
+
+class Car(Base):
+    __tablename__ = "car"
+
+    car_id = Column(BigInteger, primary_key=True)
+    car_model_id = Column(BigInteger, ForeignKey(CarModel.car_model_id), nullable=False)
+    car_owner_id = Column(BigInteger, ForeignKey(CarOwner.car_owner_id))
+    vin = Column(String(60), nullable=False)
+
+    def __repr__(self):
+        return "<Car(%r, %r, %r, %r)>" % (
+            self.car_id, self.car_model_id, self.car_owner_id, self.vin
+        )
--- Spikes/SQLAlchemy/sql_alchemy/sql_alchemy.py
+++ Spikes/SQLAlchemy/sql_alchemy/sql_alchemy.py
@@ -1,16 +1,34 @@
 from sqlite import sqlite_in_memory
-from orm_base import Base, CarMake, CarModel
+from orm_base import Base, CarMake, CarModel, CarOwner, Car
 from sqlalchemy.orm import Session
 
 Base.metadata.create_all(sqlite_in_memory.dbEngine)
 session = Session(bind=sqlite_in_memory.dbEngine)
 
-chevrolet = CarMake(car_make_id=1, car_make_name="CHEVROLET")
-toyota = CarMake(car_make_id=2, car_make_name="TOYOTA")
-session.add(chevrolet)
-session.add(toyota)
+chevrolet = CarMake(car_make_id=1, make_name="CHEVROLET")
+toyota = CarMake(car_make_id=2, make_name="TOYOTA")
+
+aveo = CarModel(car_model_id=1, car_make_id=1, model_name="AVEO")
+
+joshua = CarOwner(car_owner_id=1, first_name="Joshua", last_name="Boyd")
+
+joshuaCar = Car(car_id=1, car_model_id=1, car_owner_id=1, vin="01234567890ABCDEF0123")
+
+session.add_all([chevrolet, toyota, aveo, joshua, joshuaCar])
 
 resultSet = session.query(CarMake)
+for row in resultSet.all():
+    print(row)
+
+resultSet = session.query(CarModel)
+for row in resultSet.all():
+    print(row)
+
+resultSet = session.query(CarOwner)
+for row in resultSet.all():
+    print(row)
+
+resultSet = session.query(Car)
 for row in resultSet.all():
     print(row)
 
