b2599173c59e131fce23a833fad2a0be968b23a6
Joshua Boyd <joishi@gmail.com>
2015-11-29 22:21:07 -0800

    Moved old "Path" class that was outside of orm into orm.

--- TowerDefense/tower_defense/database/orm.py
+++ TowerDefense/tower_defense/database/orm.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, Integer, String, PrimaryKeyConstraint, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+from math import hypot
+
 SQLAlchemyBase = declarative_base()
 
 class Path(SQLAlchemyBase):
@@ -10,6 +12,19 @@ class Path(SQLAlchemyBase):
     path_id = Column(Integer, autoincrement=True, primary_key=True)
 
     name = Column(String(60), nullable=False)
+
+    def getTotalDistance(self):
+        runningTotal = 0
+        if len(self.path_points) > 0:
+            point = self.path_points[0].point
+            for nextPathPoint in self.path_points:
+                nextPoint = nextPathPoint.point
+                runningTotal += point.distance(nextPoint)
+                point = nextPoint
+
+        return runningTotal
+
+    totalDistance = property(getTotalDistance, None, None, "The total distance contained by the path")
 
     def __repr__(self):
         return "<Path(path_id=%r, name=%r)>" %(self.path_id, self.name)
@@ -26,6 +41,11 @@ class Point(SQLAlchemyBase):
     def __repr__(self):
         return "<Point(point_id=%r, x=%r, y=%r)>" %(self.point_id, self.x, self.y)
 
+    def distance(self, point):
+        dx = self.x - point.x
+        dy = self.y - point.y
+        return hypot(dx, dy)
+
 
 class PathPoint(SQLAlchemyBase):
     __tablename__ = "path_point"
@@ -36,8 +56,10 @@ class PathPoint(SQLAlchemyBase):
         PrimaryKeyConstraint(path_id, point_id),
     )
 
-    path = relationship("Path", backref="points")
-    point = relationship("Point", backref="paths")
+    path = relationship("Path", backref="path_points")
+    point = relationship("Point", backref="path_points")
 
     def __repr__(self):
         return "<PathPoint(path_id=%r, point_id=%r)>" %(self.path_id, self.point_id)
+
+
--- TowerDefense/tower_defense/test/test_path.py
+++ TowerDefense/tower_defense/test/test_path.py
@@ -1,16 +1,53 @@
 from unittest import TestCase
-from tower_defense.path import Point
+from tower_defense.database import orm
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, contains_eager
 
 
 class TestPoint(TestCase):
 
-    def test_translate(self):
-        pointToTest = Point(2, 4)
-        pointToTest.translate(5, 10)
-        self.assertEqual(pointToTest.x, 7, "X should be at 7")
-        self.assertEqual(pointToTest.y, 14, "Y should be at 14")
+    def test_orm(self):
+        pointToTest = orm.Point(x=2, y=4)
+        self.assertEqual(pointToTest.x, 2, "X should be at 2")
+        self.assertEqual(pointToTest.y, 4, "Y should be at 4")
 
-    def test_hypotenuse(self):
-        pointToTest = Point(2, 4)
-        otherPointToTest = Point(5, 8)
-        self.assertEqual(pointToTest.hypotenuse(otherPointToTest), 5, "Distance Between should be 5")
+    def test_distance(self):
+        pointToTest = orm.Point(x=2, y=4)
+        otherPointToTest = orm.Point(x=5, y=8)
+        self.assertEqual(pointToTest.distance(otherPointToTest), 5, "Distance Between should be 5")
+
+
+class TestPath(TestCase):
+
+    def test_orm(self):
+        pathToTest = orm.Path(path_id=1, name="Test Path")
+
+        dbEngine = create_engine("sqlite://")
+        orm.SQLAlchemyBase.metadata.create_all(dbEngine)
+        session = Session(bind=dbEngine)
+        session.add_all([pathToTest])
+
+        pathToTest = session.query(orm.Path).filter(orm.Path.path_id == 1).one()
+        self.assertEqual(pathToTest.name, "Test Path", 'Test Path name should be "Test Path"')
+
+    def test_horizontal_distance(self):
+        pathToTest = orm.Path(path_id=1, name="Test Path")
+        point1 = orm.Point(point_id=1, x=0, y=0)
+        point2 = orm.Point(point_id=2, x=100, y=0)
+        pathPoint1 = orm.PathPoint(path_id=pathToTest.path_id, point_id=point1.point_id)
+        pathPoint2 = orm.PathPoint(path_id=pathToTest.path_id, point_id=point2.point_id)
+        pathToTest.path_points = [pathPoint1, pathPoint2]
+
+        dbEngine = create_engine("sqlite://")
+        orm.SQLAlchemyBase.metadata.create_all(dbEngine)
+        session = Session(bind=dbEngine)
+        session.add_all([pathToTest, point1, point2, pathPoint1, pathPoint2])
+
+        pathToTest = session.query(orm.Path).\
+            join(orm.PathPoint, orm.Path.path_id == orm.PathPoint.path_id).\
+            join(orm.Point, orm.PathPoint.point_id == orm.Point.point_id).\
+            filter(orm.Path.path_id == 1).\
+            options(contains_eager(orm.Path.path_points)).\
+            one()
+        self.assertEqual(pathToTest.totalDistance, 100, "Test Path total distance should be 100")
+
