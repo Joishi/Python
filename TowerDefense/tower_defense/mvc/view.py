aab60a7ba7ed3906f9efdc1ea6465797e007f928
Joshua Boyd <joishi@gmail.com>
2015-11-29 22:32:36 -0800

    A menu under tkinter .. previous commit was supposed to be tkinter, not openGL

--- TowerDefense/tower_defense/mvc/view.py
+++ TowerDefense/tower_defense/mvc/view.py
@@ -2,20 +2,16 @@ import tkinter as glib
 
 class MainView(glib.Frame):
 
-    def print_out(self):
-        print("%s, %s!" %(self.greeting_var.get().title(), self.recipient_var.get()))
-
-    def run(self):
-        self.mainloop()
-
     def __init__(self, master):
         glib.Frame.__init__(self, master, width=800, height=600)
+        self.createMenu(master)
         self.master.title("PyDefense")
         self.pack_propagate(0)
         self.pack()
 
         self.greeting_var = glib.StringVar()
         self.greeting = glib.OptionMenu(self, self.greeting_var, "Hello", "Goodbye", "Heyo")
+        self.greeting_var.trace("w", self.updateBackground)
         self.greeting_var.set("Hello")
 
         self.recipient_var = glib.StringVar()
@@ -31,11 +27,42 @@ class MainView(glib.Frame):
         self._model = None
         self._gameStages = None
 
+    def createMenu(self, master):
+        masterMenuBar = glib.Menu(master)
+
+        fileMenu = glib.Menu(masterMenuBar, tearoff=0)
+        fileMenu.add_command(label="Exit", command=self.shutdown)
+        masterMenuBar.add_cascade(label="File", menu=fileMenu)
+
+        editMenu = glib.Menu(masterMenuBar, tearoff=0)
+        masterMenuBar.add_cascade(label="Edit", menu=editMenu)
+
+        helpMenu = glib.Menu(masterMenuBar, tearoff=0)
+        masterMenuBar.add_cascade(label="Help", menu=helpMenu)
+
+        master.config(menu=masterMenuBar)
+
+    def run(self):
+        self.mainloop()
+
+    def shutdown(self):
+        print("SHUTDOWN")
+
+    def updateBackground(self, name, index, mode):
+        print("UPDATE BACKGROUND")
+
+    def updateGameStageSelection(self):
+        print("UPDATE GAME STAGE SELECTION")
+
+    def print_out(self):
+        print("%s, %s!" %(self.greeting_var.get().title(), self.recipient_var.get()))
+
     def setModel(self, model):
         self._model = model
 
     def setAvailableGameStages(self, gameStages):
         self._gameStages = gameStages
+        self.updateGameStageSelection()
 
     model = property(None, setModel, None, "The model associated with this view")
     gameStages = property(None, setAvailableGameStages, None, "The game stages that are available to be played in this view")
