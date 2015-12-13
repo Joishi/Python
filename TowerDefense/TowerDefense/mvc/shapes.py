d133069360ecda54813b9ecfb275e19fc959c10e
Joshua Boyd <joishi@gmail.com>
2015-12-13 12:50:49 -0800

    Refactoring out shapes that will be drawn into separate file

---
+++ TowerDefense/TowerDefense/mvc/shapes.py
@@ -0,0 +1,263 @@
+# This file contains shapes to draw with openGL
+
+from collections import deque
+import math
+
+class Point3D(object):
+
+    def __init__(self, x, y, z):
+        self._x = x
+        self._y = y
+        self._z = z
+
+    def getX(self):
+        return float(self._x)
+
+    def setX(self, x):
+        self._x = x
+
+    def getY(self):
+        return float(self._y)
+
+    def setY(self, y):
+        self._y = y
+
+    def getZ(self):
+        return float(self._z)
+
+    def setZ(self, z):
+        self._z = z
+
+    x = property(getX, setX, None, "The X value for the point")
+    y = property(getY, setY, None, "The Y value for the point")
+    z = property(getZ, setZ, None, "The Z value for the point")
+
+    def distance(self, point):
+        dx = self._x - point.x
+        dy = self._y - point.y
+        dz = self._z - point.z
+        return math.hypot(math.hypot(dx, dy), math.hypot(dy, dz))
+
+    def translate(self, dx, dy, dz):
+        self._x += dx
+        self._y += dy
+        self._z += dz
+
+
+class Movable(object):
+
+    def __init__(self, startPoint, startingVelocity, maximumVelocity, acceleration):
+        self._currentLocation = startPoint
+        self._currentDestination = None
+        self._destinations = deque()
+        self._currentVelocity = startingVelocity
+        self._maximumVelocity = maximumVelocity
+        self._acceleration = acceleration
+
+    def addDestination(self, destinationPoint):
+        self._destinations.append(destinationPoint)
+
+    def move(self, elapsedTime):
+        doneMoving = True
+        if self._currentDestination is not None:
+            # adjust velocity
+            if self._currentVelocity < self._maximumVelocity:
+                self._currentVelocity += (self._acceleration * elapsedTime)
+            if self._currentVelocity > self._maximumVelocity:
+                self._currentVelocity = self._maximumVelocity
+            distanceToDestination = self._currentLocation.distance(self._currentDestination)
+            if distanceToDestination == 0:
+                self._currentDestination = None
+                doneMoving = self.move(elapsedTime)
+            else:
+                maxDistanceCanTravelThisFrame = self._currentVelocity * elapsedTime
+                ratio = maxDistanceCanTravelThisFrame / distanceToDestination
+                if ratio >= 1:
+                    # Set to destination, figure out how long that took, and move again with remaining time .. also adjust velocity so that acceleration isn't accidentally doubled
+                    self._currentLocation = self._currentDestination
+                    self._currentDestination = None
+                    remainingDistanceToTravel = maxDistanceCanTravelThisFrame - distanceToDestination
+                    ratio = remainingDistanceToTravel / maxDistanceCanTravelThisFrame
+                    newElapsedTime = elapsedTime * ratio
+                    self._currentVelocity -= (self._acceleration * newElapsedTime)
+                    doneMoving = self.move(newElapsedTime)
+                else:
+                    # Just move and be done .. return False because not done moving yet .. calculations based off of similar triangles
+                    bigDX = self._currentDestination.x - self._currentLocation.x
+                    bigDY = self._currentDestination.y - self._currentLocation.y
+                    bigDZ = self._currentDestination.z - self._currentLocation.z
+                    self._currentLocation.translate(bigDX * ratio, bigDY * ratio, bigDZ * ratio)
+                    doneMoving = False
+        else:
+            if len(self._destinations) > 0:
+                self._currentDestination = self._destinations.popleft()
+                doneMoving = self.move(elapsedTime)
+        return doneMoving
+
+    def getCurrentLocation(self):
+        return self._currentLocation
+
+    currentLocation = property(getCurrentLocation, None, None, "The current location of the movable shape")
+
+
+class Sphere(Movable):
+
+    def __init__(self, center, radius, color):
+        Movable.__init__(self, center, 10, 30, 2)
+        self._center = center
+        self._radius = radius
+        self._color = color
+        self.redGrow = True
+        self.greenGrow = True
+        self.blueGrow = True
+        self.xGrow = True
+        self.yGrow = True
+        self.zGrow = True
+
+    def getCenter(self):
+        return self._center
+
+    def setCenter(self, center):
+        self._center = center
+
+    def getRadius(self):
+        return float(self._radius)
+
+    def setRadius(self, radius):
+        self._radius = radius
+
+    def getColor(self):
+        return self._color
+
+    def setColor(self, color):
+        self._color = color
+
+    center = property(getCenter, setCenter, None, "")
+    radius = property(getRadius, setRadius, None, "")
+    color = property(getColor, setColor, None, "")
+
+    def rotateCenter(self, elapsedTime):
+        x = self._center.x
+        y = self._center.y
+        z = self._center.z
+        if x >= 10:
+            self.xGrow = False
+        if x <= -10:
+            self.xGrow = True
+        if y >= 10:
+            self.yGrow = False
+        if y <= -10:
+            self.yGrow = True
+        if z >= 10:
+            self.zGrow = False
+        if z <= -10:
+            self.zGrow = True
+        if self.xGrow:
+            x = x + .1
+        else:
+            x = x - .1
+        if self.yGrow:
+            y = y + .05
+        else:
+            y = y - .05
+        if self.zGrow:
+            z = z + .025
+        else:
+            z = z - .025
+        self._center.x = x
+        self._center.y = y
+        self._center.z = z
+
+    def rotateColor(self):
+        red = self._color.r
+        green = self._color.g
+        blue = self._color.b
+        if red >= 255:
+            self.redGrow = False
+        if red <= 0:
+            self.redGrow = True
+        if green >= 255:
+            self.greenGrow = False
+        if green <= 0:
+            self.greenGrow = True
+        if blue >= 255:
+            self.blueGrow = False
+        if blue <= 0:
+            self.blueGrow = True
+        if self.redGrow:
+            red = red + 1
+        else:
+            red = red - 1
+        if self.greenGrow:
+            green = green + 1
+        else:
+            green = green - 1
+        if self.blueGrow:
+            blue = blue + 1
+        else:
+            blue = blue - 1
+        self._color.r = red
+        self._color.g = green
+        self._color.b = blue
+
+class Color(object):
+
+    def __init__(self, red, green, blue, alpha):
+        self._red = red
+        self._green = green
+        self._blue = blue
+        self._alpha = alpha
+
+    def getRedAsPercent(self):
+        return self._red / 255.
+
+    def getGreenAsPercent(self):
+        return self._green / 255.
+
+    def getBlueAsPercent(self):
+        return self._blue / 255.
+
+    def getAlphaAsPercent(self):
+        return self._alpha / 255.
+
+    def getRed(self):
+        return self._red
+
+    def setRed(self, red):
+        if red >= 0 and red <= 255:
+            self._red = red
+        else:
+            raise ValueError("Red attribute of color must be at least 0 and at most 255")
+
+    def getGreen(self):
+        return self._green
+
+    def setGreen(self, green):
+        if green >= 0 and green <= 255:
+            self._green = green
+        else:
+            raise ValueError("Green attribute of color must be at least 0 and at most 255")
+
+    def getBlue(self):
+        return self._blue
+
+    def setBlue(self, blue):
+        if blue >= 0 and blue <= 255:
+            self._blue = blue
+        else:
+            raise ValueError("Blue attribute of color must be at least 0 and at most 255")
+
+    def getAlpha(self):
+        return self._alpha
+
+    def setAlpha(self, alpha):
+        if alpha >= 0 and alpha <= 255:
+            self._alpha = alpha
+        else:
+            raise ValueError("Alpha attribute of color must be at least 0 and at most 255")
+
+    r = property(getRed, setRed, None, "The red value of the color")
+    g = property(getGreen, setGreen, None, "The green value of the color")
+    b = property(getBlue, setBlue, None, "The blue value of the color")
+    a = property(getAlpha, setAlpha, None, "The opaqueness value of the color")
+
--- TowerDefense/TowerDefense/mvc/view.py
+++ TowerDefense/TowerDefense/mvc/view.py
@@ -1,7 +1,8 @@
-﻿import sys
+﻿import time, sys
 import OpenGL.GL as gl
 import OpenGL.GLU as glu
 import OpenGL.GLUT as glut
+from mvc.shapes import *
 
 class ModelListener(object):
     '''To properly implement this class, you will need to add the following functions to your class:
@@ -24,10 +25,15 @@ class MainView(ModelListener):
         self._eyePosition = None
         self._watchingPosition = None
         self._upVector = None
+        self._lastTime = None
 
         sphereCenter = Point3D(0, 0, 0)
+        sphereRadius = 20
         sphereColor = Color(255, 127, 0, 255)
-        self.sphere = Sphere(sphereCenter, 2, sphereColor)
+        self.sphere = Sphere(sphereCenter, sphereRadius, sphereColor)
+        self.sphere.addDestination(Point3D(50, 50, 50))
+        self.sphere.addDestination(Point3D(-50, -50, -50))
+        self.sphere.addDestination(Point3D(0, 0, 0))
 
     def show(self):
         self.initOpenGLMatrix()
@@ -58,19 +64,19 @@ class MainView(ModelListener):
         gl.glEnable(gl.GL_CULL_FACE)
         gl.glEnable(gl.GL_DEPTH_TEST)
         gl.glEnable(gl.GL_LIGHTING)
-        gl.lightZeroPosition = [10., 4., 10., 1.]
+        gl.lightZeroPosition = [100., 40., 100., 1.]
         gl.lightZeroColor = [0.8, 1.0, 0.8, 1.0] #green tinged
         gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, gl.lightZeroPosition)
         gl.glLightfv(gl.GL_LIGHT0, gl.GL_DIFFUSE, gl.lightZeroColor)
-        gl.glLightf(gl.GL_LIGHT0, gl.GL_CONSTANT_ATTENUATION, 0.1)
-        gl.glLightf(gl.GL_LIGHT0, gl.GL_LINEAR_ATTENUATION, 0.05)
+        gl.glLightf(gl.GL_LIGHT0, gl.GL_CONSTANT_ATTENUATION, 0.01)
+        gl.glLightf(gl.GL_LIGHT0, gl.GL_LINEAR_ATTENUATION, 0.005)
         gl.glEnable(gl.GL_LIGHT0)
         glut.glutDisplayFunc(self.paint)
         glut.glutIdleFunc(self.repaint)
         gl.glMatrixMode(gl.GL_PROJECTION)
-        glu.gluPerspective(40., 1., 1., 40.)
+        glu.gluPerspective(30., 1., 1., 1000.)
         gl.glMatrixMode(gl.GL_MODELVIEW)
-        self._eyePosition = Point3D(0, 0, 10)
+        self._eyePosition = Point3D(0, 0, 400)
         self._watchingPosition = Point3D(0, 0, 0)
         self._upVector = Point3D(0, 1, 0)
         glu.gluLookAt(self._eyePosition.x, self._eyePosition.y, self._eyePosition.z,
@@ -79,23 +85,30 @@ class MainView(ModelListener):
         gl.glPushMatrix()
 
     def paint(self):
+        elapsedTime = 0
+        currentTime = time.clock()
+        if self._lastTime is not None:
+            elapsedTime = currentTime - self._lastTime
+        self._lastTime = currentTime
         if self._model is not None:
             if self._model.activeGameStage is not None:
                 path = self._model.activeGameStage.path
-                for point in path.points:
-                    print(point)
+#                for point in path.points:
+#                    print(point)
         gl.glPopMatrix()
         gl.glPushMatrix()
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-        sphereCenter = self.sphere.center
+#        sphereCenter = self.sphere.center
+        sphereCenter = self.sphere.currentLocation
         sphereRadius = self.sphere.radius
         sphereColor = self.sphere.color
         gl.glTranslate(sphereCenter.x, sphereCenter.y, sphereCenter.z)
         gl.glMaterialfv(gl.GL_FRONT, gl.GL_DIFFUSE, [sphereColor.getRedAsPercent(), sphereColor.getGreenAsPercent(), sphereColor.getBlueAsPercent(), sphereColor.getAlphaAsPercent()])
-        glut.glutSolidSphere(sphereRadius, 50, 50)
+        glut.glutSolidSphere(sphereRadius, 15, 15)
         glut.glutSwapBuffers()
         self.sphere.rotateColor()
-        self.sphere.rotateCenter()
+        self.sphere.move(elapsedTime)
+#        self.sphere.rotateCenter()
         return
 
     def repaint(self):
@@ -108,195 +121,4 @@ class MainView(ModelListener):
             print("  " + str(gameStage.path))
             for point in gameStage.path.points:
                 print("    " + str(point))
-
-
-class Sphere(object):
-
-    def __init__(self, center, radius, color):
-        self._center = center
-        self._radius = radius
-        self._color = color
-        self.redGrow = True
-        self.greenGrow = True
-        self.blueGrow = True
-        self.xGrow = True
-        self.yGrow = True
-        self.zGrow = True
-
-    def getCenter(self):
-        return self._center
-
-    def setCenter(self, center):
-        self._center = center
-
-    def getRadius(self):
-        return float(self._radius)
-
-    def setRadius(self, radius):
-        self._radius = radius
-
-    def getColor(self):
-        return self._color
-
-    def setColor(self, color):
-        self._color = color
-
-    center = property(getCenter, setCenter, None, "")
-    radius = property(getRadius, setRadius, None, "")
-    color = property(getColor, setColor, None, "")
-
-    def rotateCenter(self):
-        x = self._center.x
-        y = self._center.y
-        z = self._center.z
-        if x >= 10:
-            self.xGrow = False
-        if x <= -10:
-            self.xGrow = True
-        if y >= 10:
-            self.yGrow = False
-        if y <= -10:
-            self.yGrow = True
-        if z >= 10:
-            self.zGrow = False
-        if z <= -10:
-            self.zGrow = True
-        if self.xGrow:
-            x = x + .1
-        else:
-            x = x - .1
-        if self.yGrow:
-            y = y + .05
-        else:
-            y = y - .05
-        if self.zGrow:
-            z = z + .025
-        else:
-            z = z - .025
-        self._center.x = x
-        self._center.y = y
-        self._center.z = z
-
-    def rotateColor(self):
-        red = self._color.r
-        green = self._color.g
-        blue = self._color.b
-        if red >= 255:
-            self.redGrow = False
-        if red <= 0:
-            self.redGrow = True
-        if green >= 255:
-            self.greenGrow = False
-        if green <= 0:
-            self.greenGrow = True
-        if blue >= 255:
-            self.blueGrow = False
-        if blue <= 0:
-            self.blueGrow = True
-        if self.redGrow:
-            red = red + 1
-        else:
-            red = red - 1
-        if self.greenGrow:
-            green = green + 1
-        else:
-            green = green - 1
-        if self.blueGrow:
-            blue = blue + 1
-        else:
-            blue = blue - 1
-        self._color.r = red
-        self._color.g = green
-        self._color.b = blue
-
-class Point3D(object):
-
-    def __init__(self, x, y, z):
-        self._x = x
-        self._y = y
-        self._z = z
-
-    def getX(self):
-        return float(self._x)
-
-    def setX(self, x):
-        self._x = x
-
-    def getY(self):
-        return float(self._y)
-
-    def setY(self, y):
-        self._y = y
-
-    def getZ(self):
-        return float(self._z)
-
-    def setZ(self, z):
-        self._z = z
-
-    x = property(getX, setX, None, "The X value for the point")
-    y = property(getY, setY, None, "The Y value for the point")
-    z = property(getZ, setZ, None, "The Z value for the point")
-
-
-class Color(object):
-
-    def __init__(self, red, green, blue, alpha):
-        self._red = red
-        self._green = green
-        self._blue = blue
-        self._alpha = alpha
-
-    def getRedAsPercent(self):
-        return self._red / 255.
-
-    def getGreenAsPercent(self):
-        return self._green / 255.
-
-    def getBlueAsPercent(self):
-        return self._blue / 255.
-
-    def getAlphaAsPercent(self):
-        return self._alpha / 255.
-
-    def getRed(self):
-        return self._red
-
-    def setRed(self, red):
-        if red >= 0 and red <= 255:
-            self._red = red
-        else:
-            raise ValueError("Red attribute of color must be at least 0 and at most 255")
-
-    def getGreen(self):
-        return self._green
-
-    def setGreen(self, green):
-        if green >= 0 and green <= 255:
-            self._green = green
-        else:
-            raise ValueError("Green attribute of color must be at least 0 and at most 255")
-
-    def getBlue(self):
-        return self._blue
-
-    def setBlue(self, blue):
-        if blue >= 0 and blue <= 255:
-            self._blue = blue
-        else:
-            raise ValueError("Blue attribute of color must be at least 0 and at most 255")
-
-    def getAlpha(self):
-        return self._alpha
-
-    def setAlpha(self, alpha):
-        if alpha >= 0 and alpha <= 255:
-            self._alpha = alpha
-        else:
-            raise ValueError("Alpha attribute of color must be at least 0 and at most 255")
-
-    r = property(getRed, setRed, None, "The red value of the color")
-    g = property(getGreen, setGreen, None, "The green value of the color")
-    b = property(getBlue, setBlue, None, "The blue value of the color")
-    a = property(getAlpha, setAlpha, None, "The opaqueness value of the color")
 
